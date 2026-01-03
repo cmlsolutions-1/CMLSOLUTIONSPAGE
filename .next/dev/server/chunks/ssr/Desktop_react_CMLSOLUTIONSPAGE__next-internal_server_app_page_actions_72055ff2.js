ca88220cff25034ca32b9d89d0952af9a7eee1de
ING-LENIN <lenina.moraleso@uqvirtual.edu.co>
2026-01-02 19:45:13 -0500

    fix: proper gitignore and clean next build files

--- .next/dev/server/chunks/ssr/Desktop_react_CMLSOLUTIONSPAGE__next-internal_server_app_page_actions_72055ff2.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/Desktop/react/CMLSOLUTIONSPAGE/.next-internal/server/app/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=Desktop_react_CMLSOLUTIONSPAGE__next-internal_server_app_page_actions_72055ff2.js.map
--- .next/dev/static/chunks/Desktop_react_CMLSOLUTIONSPAGE_app_layout_tsx_7ac29437._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__74048464._.css",
-  "static/chunks/79821_a2ad55d3._.js"
-],
-    source: "dynamic"
-});
